3c9f0bcc60b8ac90a52afa81fea3fa2f8d118686
neronmoon <alistar.neron@gmail.com>
2025-01-20 22:20:40 +0200

    Fix tests, recompile protocol files, fix requirements

--- leea_agent_sdk/web3.py
+++ leea_agent_sdk/web3.py
@@ -1,3 +1,5 @@
+import os.path
+
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
 from web3 import Web3
@@ -13,14 +15,12 @@ class Web3Instance:
     account: LocalAccount
     w3: Web3
 
-    def __init__(self, keystorePath: str, keystorePassword: str):
-        self.path = keystorePath
-        self.password = keystorePassword
+    def __init__(self, keystore_path: str, keystore_password: str):
+        self.path = keystore_path
+        self.password = keystore_password
 
     def create_wallet(self):
-        try:
-            keyfile = open(self.path)
-        except OSError:
+        if not os.path.isfile(self.path):
             logger.info("Could not open/read keystore file, creating a new one")
             self.account: LocalAccount = Account.create(urandom(256))
             logger.info(f"New account created: {self.account.address}")
@@ -28,29 +28,31 @@ class Web3Instance:
             with open(self.path, "w") as f:
                 f.write(json.dumps(encrypted))
                 logger.info(f"New account was saved as file: {self.path}")
-                f.close()
             return
 
         with open(self.path) as keyfile:
-            encryptedFile = json.load(keyfile)
-            priv_key = Account.decrypt(encryptedFile, self.password)
-            self.account: LocalAccount = Account.from_key(priv_key)
+            encrypted_file = json.load(keyfile)
+            private_key = Account.decrypt(encrypted_file, self.password)
+            self.account: LocalAccount = Account.from_key(private_key)
             logger.info(f"Using existing account: {self.account.address}")
             keyfile.close()
 
-    def connect(self, endpointURL: str):
-        w3: Web3 = Web3(Web3.HTTPProvider(endpointURL))
+    def connect(self, endpoint: str):
+        w3: Web3 = Web3(Web3.HTTPProvider(endpoint))
         w3.middleware_onion.inject(
             SignAndSendRawMiddlewareBuilder.build(self.account), layer=0
         )
         self.w3 = w3
+        return self.w3.is_connected()
 
     def sign_message(self, msg: str) -> str:
-        signedMessage = self.account.sign_message(encode_defunct(keccak(text=msg)))
-        return signedMessage.signature.to_0x_hex()
+        signed_msg = self.account.sign_message(encode_defunct(keccak(text=msg)))
+        return signed_msg.signature.to_0x_hex()
 
     def verify_message(self, msg: str, signature: str) -> bool:
-        result: bool = Account.recover_message(
-            encode_defunct(keccak(text=msg)), signature=signature
-        )
-        return result
+        try:
+            Account.recover_message(encode_defunct(keccak(text=msg)), signature=signature)
+            return True
+        except Exception as e:
+            logger.exception(e)
+            return False
