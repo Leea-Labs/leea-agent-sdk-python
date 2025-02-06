5bf234917989290c62c79d66022ea112285a0460
neronmoon <alistar.neron@gmail.com>
2025-02-06 02:39:06 +0200

    Protocon on protobuf

---
+++ tests/unit/protocol/test_protocol.py
@@ -0,0 +1,10 @@
+from leea_agent_sdk.protocol import pack, unpack
+from leea_agent_sdk.protocol.protocol_pb2 import ExecutionResult
+
+
+def test_protocol_packing():
+    packed = pack(ExecutionResult(RequestID="2", ))
+    assert type(packed) == bytes
+    unpacked = unpack(packed)
+    assert isinstance(unpacked, ExecutionResult)
+    assert unpacked.RequestID == '2'
