a2968a773a6f39ff1be04af237214756ae03732b
neronmoon <alistar.neron@gmail.com>
2025-02-06 08:04:20 +0200

    Runtime and parallel execution

--- leea_agent_sdk/transport.py
+++ leea_agent_sdk/transport.py
@@ -1,11 +1,10 @@
-import json
+import asyncio
 from functools import wraps
+from os import getenv
 
-from websockets import ConnectionClosedError
-from websockets.sync.client import connect as ws_connect
+from websockets import ConnectionClosedError, connect as ws_connect
 
 from leea_agent_sdk.logger import logger
-from leea_agent_sdk.protocol import messages, ProtoMessage
 
 
 def reconnect_if_closed(method):
@@ -14,27 +13,48 @@ def reconnect_if_closed(method):
         try:
             return method(self, *args, **kwargs)
         except ConnectionClosedError:
-            logger.warning("Connection error, reconnecting...")
-            self.connection = self.connect()
+            logger.warn("Connection error, will reconnect")
+            self._connection = None
             return method(self, *args, **kwargs)
 
     return wrapper
 
 
 class Transport:
-    def __init__(self, agent_name):
-        logger.info(f"Connecting {agent_name}")
-        self.agent_name = agent_name
-        self.connection = self.connect()
+    _connection = None
+    _connect_callbacks = []
 
-    def connect(self):
-        return ws_connect("ws://localhost:8081/api/v1/connect", additional_headers={"AGENT_NAME": self.agent_name})
+    def __init__(self):
+        self._connect_uri = f"{getenv("LEEA_API_WS_HOST", "ws://localhost:8081")}/api/v1/connect"
+
+    def on_connect(self, callback):
+        self._connect_callbacks.append(callback)
+
+    async def _get_connection(self):
+        if self._connection is not None:
+            return self._connection
+
+        logger.debug(f"Connecting: {self._connect_uri}")
+        self._connection = ws_connect(
+            self._connect_uri,
+            ping_interval=5,
+            ping_timeout=1
+        )
+        for cb in self._connect_callbacks:
+            if asyncio.iscoroutinefunction(cb):
+                await cb(self)
+            else:
+                cb(self)
+
+        return self._connection
 
     @reconnect_if_closed
-    def send(self, msg: ProtoMessage):
-        self.connection.send(msg.model_dump_json())
+    async def send(self, msg: bytes):
+        (await self._get_connection()).send(msg)
+        logger.debug(f"-> {msg}")
 
     @reconnect_if_closed
-    def receive(self) -> ProtoMessage:
-        data = json.loads(self.connection.recv())
-        return messages.validate_json(data)
+    async def receive(self) -> bytes:
+        recv = (await self._get_connection()).recv()
+        logger.debug(f"<- {recv}")
+        return recv
