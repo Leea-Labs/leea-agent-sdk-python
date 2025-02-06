e4c6b089bec41f0c54a09cd902e48be8e94f8fcc
neronmoon <alistar.neron@gmail.com>
2025-02-06 21:01:17 +0200

    Fix linting

--- tests/unit/protocol/test_protocol.py
+++ tests/unit/protocol/test_protocol.py
@@ -4,7 +4,7 @@ from leea_agent_sdk.protocol.protocol_pb2 import ExecutionResult
 
 def test_protocol_packing():
     packed = pack(ExecutionResult(RequestID="2", ))
-    assert type(packed) == bytes
+    assert isinstance(packed, bytes)
     unpacked = unpack(packed)
     assert isinstance(unpacked, ExecutionResult)
     assert unpacked.RequestID == '2'
