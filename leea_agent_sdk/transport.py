3c71d99dfe66c52adff94a0be584ec983c54d407
neronmoon <alistar.neron@gmail.com>
2025-02-08 01:44:14 +0200

    Message signing and sign verification

--- leea_agent_sdk/transport.py
+++ leea_agent_sdk/transport.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import time
 from functools import wraps
 from os import getenv
@@ -7,9 +8,11 @@ from websockets.exceptions import ConnectionClosedError
 from websockets.asyncio.client import connect
 from websockets.protocol import State
 from google.protobuf import message as _message
-import leea_agent_sdk.protocol as protocol
 
 from leea_agent_sdk.logger import logger
+from leea_agent_sdk.protocol.protocol_pb2 import DESCRIPTOR, Envelope
+from leea_agent_sdk.web3 import Web3Instance
+from google.protobuf import message_factory
 
 
 def reconnect_if_closed(method):
@@ -32,6 +35,7 @@ class Transport:
     def __init__(self, api_key=None):
         self._connect_uri = f"{getenv('LEEA_API_WS_HOST', 'ws://localhost:8081')}/api/v1/connect"
         self._api_key = api_key or getenv("LEEA_API_KEY")
+        self._wallet = Web3Instance(getenv('LEEA_WALLET_PATH', f"{os.getcwd()}/wallet.json"))
         if not self._api_key:
             raise RuntimeError("Please provide LEEA_API_KEY")
 
@@ -74,13 +78,38 @@ class Transport:
 
     @reconnect_if_closed
     async def send(self, msg: _message.Message):
-        packed = protocol.pack(msg)
+        packed = self._pack(msg)
         await (await self._get_connection()).send(packed)
         logger.debug(f"-> {msg}")
 
     @reconnect_if_closed
     async def receive(self) -> _message.Message:
         recv = await (await self._get_connection()).recv()
-        msg = protocol.unpack(recv)
+        msg = self._unpack(recv)
         logger.debug(f"<- {msg}")
         return msg
+
+    def get_public_key(self):
+        return self._wallet.get_public_key()
+
+    def _pack(self, message: _message.Message) -> bytes:
+        payload = message.SerializeToString()
+        public_key, signature = self._wallet.sign_message(payload)
+        envelope = Envelope(
+            Type=Envelope.MessageType.Value(message.DESCRIPTOR.name),
+            Payload=payload,
+            PublicKey=public_key,
+            Signature=signature
+        )
+        return envelope.SerializeToString()
+
+    def _unpack(self, data: bytes) -> _message.Message:
+        envelope = Envelope()
+        envelope.ParseFromString(data)
+        assert self._wallet.verify_message(envelope.Payload, envelope.Signature), "Message signature is not valid"
+
+        message_type = Envelope.MessageType.Name(envelope.Type)
+        message_type = DESCRIPTOR.message_types_by_name[message_type]
+        message = message_factory.GetMessageClass(message_type)()
+        message.ParseFromString(envelope.Payload)
+        return message
