f0ba57a2934c1420db91a1c868a6bb1a263d4bd6
pavelkrolevets <pavelkrolevets@gmail.com>
2025-02-08 21:02:07 +0200

    fix sign envelop

--- tests/unit/test_web3_solana.py
+++ tests/unit/test_web3_solana.py
@@ -3,9 +3,9 @@ from leea_agent_sdk.web3_solana import Web3InstanceSolana
 
 def test_sign_verify():
     inst = Web3InstanceSolana("tests/unit/fixtures/id.json")
-    message = "The quick brown fox jumps over the lazy dog"
+    message = b"The quick brown fox jumps over the lazy dog"
     sig = inst.sign_message(message)
-    agent_id = inst.get_agent_id()
+    agent_id = inst.get_public_key()
     ver_res = inst.verify_message(agent_id, message, sig)
     assert ver_res is True
 
