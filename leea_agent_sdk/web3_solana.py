c95affcc9ad86aeffb73840a3e3958fd9444668f
pavelkrolevets <pavelkrolevets@gmail.com>
2025-02-12 13:23:04 +0200

    fix sign envelop

--- leea_agent_sdk/web3_solana.py
+++ leea_agent_sdk/web3_solana.py
@@ -1,8 +1,8 @@
-from eth_utils import keccak
 from solana.rpc.api import Client
 from solders.keypair import Keypair
 from solders.signature import Signature
 from solders.pubkey import Pubkey
+
 
 class Web3InstanceSolana:
     client: Client
@@ -14,16 +14,16 @@ class Web3InstanceSolana:
             self.keypair = Keypair.from_json(data)
             assert self.keypair.pubkey().is_on_curve()
 
-    def get_agent_id(self) -> str:
+    def get_public_key(self) -> str:
         return self.keypair.pubkey().__str__()
 
-    def sign_message(self, msg: str) -> str:
-        return self.keypair.sign_message(keccak(text=msg)).__str__()
-    
-    def verify_message(self, agent_id: str, msg: str, sig: str) -> bool:
-        pubkey: Pubkey = Pubkey.from_string(agent_id)
+    def sign_message(self, msg: bytes) -> str:
+        return self.keypair.sign_message(msg).__str__()
+
+    def verify_message(self, pub_key: str, msg: bytes, sig: str) -> bool:
+        pubkey: Pubkey = Pubkey.from_string(pub_key)
         signature: Signature = Signature.from_string(sig)
-        return signature.verify(pubkey, keccak(text=msg))
+        return signature.verify(pubkey, msg)
 
     def connect(self, url: str) -> bool:
         self.client = Client(url)
